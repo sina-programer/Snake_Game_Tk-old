fc3fed01b1ae06185f15a12321e6932a0fbdeb73
mohammadamin <mohammadamin>
2021-10-04 17:05:25 +0330

    fix body add and move

--- Snake Game Tk.py
+++ Snake Game Tk.py
@@ -12,20 +12,21 @@ class PlayComponent:
     def move(self, x, y):
         self.canvas.move(self.snake_head, x, y)
 
-    def move_body(self, body, snake_history_of_move):
+    def move_body(self, body, snake_history_of_move, aim):
         """
         This move the body of snake
         The snake_history_of_move must be tuple or list of coords
         :param body:
         :param snake_history_of_move:
+        :param aim:
         :return:
         """
 
         for i in range(len(body)):
-            x, y = self.get_position(body[i])  # a body position
-            x, y = self.get_next_position(snake_history_of_move, (x, y))
-            self.canvas.move(body[i], x, y)
-            # print(x, y, 'moved')
+            first_position = self.get_position(body[i])  # a body position
+            next_position = self.get_next_position(snake_history_of_move, (first_position[0], first_position[1]))
+            final_position = (next_position[0] - first_position[0], next_position[1] - first_position[1])
+            self.canvas.move(body[i], *final_position)
 
     @staticmethod
     def get_next_position(snake_history_of_move, position):
@@ -35,7 +36,7 @@ class PlayComponent:
                 return p
             if position == p:
                 is_found = True
-        return position
+        return snake_history_of_move[-1]
 
     def get_coords(self):
         return self.canvas.coords(self.snake_head)
@@ -93,7 +94,7 @@ class Snake(PlayComponent):
         aim = self.aims.get(self.direction)
         if aim is not None:
             super().move(*aim)
-            super().move_body(self.body, self.history_of_move)
+            super().move_body(self.body, self.history_of_move, aim)
 
     def save_move(self, position):
         if self.history_of_move[-1] != position:
@@ -160,9 +161,7 @@ class Game(Frame):
 
             self.snake.move()
             self.snake.save_move(self.snake.get_position(self.snake.snake_head))
-            self.snake.move_body(self.snake.body, self.snake.history_of_move)
             sleep(.15)
-
 
 
 if __name__ == "__main__":
