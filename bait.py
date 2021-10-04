300fbb5b76168d65a6a7b382642043f97b479ea3
Sina.f <sina.programerr@gmail.com>
2021-10-04 09:55:47 -0600

    Update bait.py

--- bait.py
+++ bait.py
@@ -1,22 +1,24 @@
-from base import PlayComponent
 import random
 
-
-class Bait(PlayComponent):
+class Bait:
     def __init__(self, canvas):
         self.x = random.randrange(10, 520, 15) - 2.5
         self.y = random.randrange(10, 520, 15) - 2.5
+        self.canvas = canvas
         self.size = 15
 
-        item = canvas.create_rectangle(self.x - self.size / 2,
-                                       self.y - self.size / 2,
-                                       self.x + self.size / 2,
-                                       self.y + self.size / 2,
-                                       fill='green')
-
-        super(Bait, self).__init__(canvas, item)
-
+        self.item = canvas.create_rectangle(self.x - self.size / 2,
+                                            self.y - self.size / 2,
+                                            self.x + self.size / 2,
+                                            self.y + self.size / 2,
+                                            fill='green')
+        
     def move(self):
-        self.x = random.randrange(10, 520, 15) - self.x - 2.5
-        self.y = random.randrange(10, 520, 15) - self.y - 2.5
-        super().move(self.x, self.y)
+        self.canvas.move(self.item, -self.x, -self.y)
+        self.x = random.randrange(10, 520, 15) - 2.5
+        self.y = random.randrange(10, 520, 15) - 2.5
+        self.canvas.move(self.item, self.x, self.y)
+
+    def get_position(self):
+        coords = self.canvas.coords(self.item)
+        return (coords[2] - coords[0]) / 2 + coords[0], (coords[3] - coords[1]) / 2 + coords[1]
