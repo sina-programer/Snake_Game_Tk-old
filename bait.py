172d6ce858a764d4633c989fb0bd14073cef2ba8
Sina.f <sina.programerr@gmail.com>
2021-10-05 05:33:05 -0600

    Update bait.py

--- bait.py
+++ bait.py
@@ -8,14 +8,14 @@ class Bait:
         self.x = random.randrange(10, 520, 15) - 2.5
         self.y = random.randrange(10, 520, 15) - 2.5
         self.canvas = canvas
-        self.time_to_move = 12 - level * 2
+        self.time_to_move = 12 - level*2
         self.timer = self.time_to_move
         self.size = 15
 
-        self.item = canvas.create_rectangle(self.x - self.size / 2,
-                                            self.y - self.size / 2,
-                                            self.x + self.size / 2,
-                                            self.y + self.size / 2,
+        self.item = canvas.create_rectangle(self.x - self.size/2,
+                                            self.y - self.size/2,
+                                            self.x + self.size/2,
+                                            self.y + self.size/2,
                                             fill='green')
 
         Thread(target=self.auto_move).start()
@@ -26,10 +26,13 @@ class Bait:
         self.x = random.randrange(10, 520, 15) - 2.5
         self.y = random.randrange(10, 520, 15) - 2.5
         self.canvas.move(self.item, self.x, self.y)
+        
+    def reset(self):
+        self.timer = self.time_to_move
 
     def get_position(self):
         coords = self.canvas.coords(self.item)
-        return (coords[2] - coords[0]) / 2 + coords[0], (coords[3] - coords[1]) / 2 + coords[1]
+        return (coords[2] - coords[0])/2 + coords[0], (coords[3] - coords[1])/2 + coords[1]
 
     def auto_move(self):
         while True:
