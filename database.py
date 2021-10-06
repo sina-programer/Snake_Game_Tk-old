5c6e53069cc7a78793c24a550855919a4f6ee750
Sina.f <sina.programerr@gmail.com>
2021-10-06 11:12:52 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,22 @@
 import peewee as pw
 
 db = pw.SqliteDatabase('database.db')
+db.connect()
 
 class User(pw.Model):
     name = pw.CharField()
     best_score = pw.IntegerField()
+    
+    @staticmethod
+    def connect():
+        db.connect()
+        
+    @staticmethod
+    def close():
+        db.close()
 
     class Meta:
         database = db
+
+if not db.table_exists('User'):
+    db.create_tables([User])
