d2f801aa1e308deef673d9ad1b40fb5211f08e57
mohammadamin <mohammadamin>
2021-10-06 17:30:35 +0330

    Create initialize_db function on database.py and some changes

--- database.py
+++ database.py
@@ -2,9 +2,16 @@ import peewee as pw
 
 db = pw.SqliteDatabase('database.db')
 
+
 class User(pw.Model):
     name = pw.CharField()
     best_score = pw.IntegerField()
 
     class Meta:
         database = db
+
+
+def initialize_db():
+    db.connect()
+    db.create_tables([User], safe=True)
+    db.close()
